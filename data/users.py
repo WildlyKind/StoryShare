17b9e7759a217b6f3b447fb27b0ca245ae7f6dd3
WildlyKind <python545420@gmail.com>
2025-05-06 18:33:42 +0300

    Add files via upload

--- data/users.py
+++ data/users.py
@@ -3,7 +3,6 @@ import sqlalchemy
 from flask_login import UserMixin
 from sqlalchemy import orm
 from werkzeug.security import generate_password_hash, check_password_hash
-
 from .db_session import SqlAlchemyBase
 
 
@@ -19,7 +18,7 @@ class User(SqlAlchemyBase, UserMixin):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
-    news = orm.relationship("Story", back_populates='user')
+    story = orm.relationship("Story", back_populates='user')
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
